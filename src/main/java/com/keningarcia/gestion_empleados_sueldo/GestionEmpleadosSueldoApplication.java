b61e6a9f6df428688b2f8da8bd034c0bb2f1b6e2
keningarcia <kenin.garciacampos@gmail.com>
2026-06-21 23:25:25 -0500

    Colocando la estructura de las carpetas del proyecto

---
+++ src/main/java/com/keningarcia/gestion_empleados_sueldo/GestionEmpleadosSueldoApplication.java
@@ -0,0 +1,13 @@
+package com.keningarcia.gestion_empleados_sueldo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionEmpleadosSueldoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionEmpleadosSueldoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/keningarcia/gestion_empleados_sueldo/GestionEmpleadosSueldoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keningarcia.gestion_empleados_sueldo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionEmpleadosSueldoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
